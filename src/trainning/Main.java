ffe9afc5c7d7db2a2a53f8700c9efcd00f268349
Mahdi Khrrousheh <mahdi@megit.com>
2024-09-18 23:42:44 +0300

    training: basic programming and string manipulation

---
+++ src/trainning/Main.java
@@ -0,0 +1,9 @@
+package trainning;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/trainning/StringExercises.java
@@ -0,0 +1,72 @@
+package trainning;
+
+public class StringExercises {
+    public static boolean isPalindrome(String word){
+        /*
+         * TODO: palindrome
+         * Palindrome is the words that you can read from right to left and left to right
+         * if the word is not a Palindrome print the index
+         */
+        return false;
+    }
+
+    public static boolean isAnagrams(String wordOne, String wordTwo){
+        /*
+         * TODO: anagrams
+         * anagrams is a word or phrase formed by rearranging the letters of a different word or phrase
+         *
+         */
+        return  false;
+    }
+
+    public static boolean isVariableName(String str){
+        /*
+         *  TODO: Variable name
+         * all the programming languages have a rules
+         * in this task you will take a string
+         * return a boolean indicate if that string is can be a variable name
+         *
+         */
+        return false;
+    }
+
+    public static String getCamelCaseConventions(String str){
+        /*
+         * TODO: camel case
+         * in java we follow the camel name variable convention
+         * so in this exercise you will get a string and convert it into camel case then check if it can be a variable name
+         *
+         */
+        return "";
+    }
+
+    public static String namingConventions(String str){
+        /*
+         *
+         * every programming languages have a name conventions
+         * convert the string from camel case convention to snake case convention
+         */
+        return "";
+    }
+
+    public static String[] getNames(String[] arr, int n){
+        /*
+         *  TODO: Random name
+         *  Write a function generated a random names
+         *  arr: array of string contains names
+         *  n: the length of returned lest
+         *
+         *  condition:
+         *      1. result name is constructed of two parts
+         *      2. result name parts is unique
+         *      3. create a custom exception if n is more than what you can generate
+         *
+         *  example:
+         *      arr = ["Rami", "Ahmad", "Sameh"] n = 3
+         *   result:
+         *      ["Rami Ahmad", "Rami Sameh", "Ahmad Sameh"]
+         */
+
+        return arr;
+    }
+}
